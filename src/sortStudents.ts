be7f65b29d3aa00d3ef7c97e158895bd9090d778
Leonid Vegera <leonid.vegera@gmail.com>
2021-12-09 20:31:54 +0200

    Done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const averGrade = (student: Student): number => {
+  const everSum = student.grades.reduce(
+    (sum: number, cur: number) => sum + cur, 0,
+  );
+  const { length } = student.grades;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return everSum / length;
+};
+
+export function sortStudents(
+  students: Array<Student>,
+  sortBy: SortType,
+  order: SortOrder,
+): Array<Student> {
+  const isOrdered: number = (order === 'asc' ? 1 : -1);
+
+  return [...students].sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return a.name.localeCompare(b.name) * isOrdered;
+
+      case SortType.Surname:
+        return a.surname.localeCompare(b.surname) * isOrdered;
+
+      case SortType.Age:
+        return (a.age - b.age) * isOrdered;
+
+      case SortType.Married:
+        return ((+a.married) - (+b.married)) * isOrdered;
+
+      case SortType.AverageGrade:
+        return (averGrade(a) - averGrade(b)) * isOrdered;
+
+      default:
+        return 0;
+    }
+  });
 }
